b1e6a1792f3130ae87668adeefd446e1e4db6334
EvertonAraujo19 <evertonaraujovdd@gmail.com>
2024-07-27 11:44:50 -0300

    variaveis numerais

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,13 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        byte idade = 54;
+        short ano = 2021;
+        long cpf = 15717457830L;
+
+
+        System.out.println(idade);
+        System.out.println(ano);
+        System.out.println(cpf);
     }
+
 }
